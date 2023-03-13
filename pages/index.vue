25d8731e26b7268a6c8c4a9a116dd9e6ed64d2fc
soe-k8 <soe@mail-k8.io>
2023-03-13 17:30:12 +0800

    changed the links

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,9 @@
     <p class="text-white text-base align-center my-9 text-xl">Can't access k8.io? Try one of these mirror sites below:</p>
     <div class="mt-4">
       <div class="flex flex-row items-center justify-center mb-3" v-for="(val,key) in urlList">
-        <a :href="val.url" target="_blank" style="width: 200px" class="text-white mr-9" :class="val.type == 'title'? 'decoration-none' : ''">
-          <h2 class="capitalize" :class="val.type == 'title'? 'text-xl' : 'text-base'">{{val.name}}</h2>
+        <h2 class="text-white mr-9 w-51 text-xl" v-if="val.type == 'title'">{{val.name}}</h2>
+        <a :href="val.url" target="_blank" class="text-white mr-9 w-51" v-else>
+          <h2 class="text-base">{{val.name}}</h2>
         </a>
 <!--        <span class="bg-dark-100 p-13px rounded-5px inline-block w-52 align-center">-->
 <!--          <a href="https://k8.io/promotions" class="text-swam-white-600 text-13px decoration-none">{{val.url}}</a>-->
@@ -92,37 +93,37 @@ useHead({
 
 const urlList = [
   {
-    name: "sites",
+    name: "Sites",
     url: "https://k8.io",
     status: 'Status',
     type:'title',
   },
   {
-    name: "K8",
+    name: "K8.io",
     url: "https://k8.io",
     status: "online",
     type:'content',
   },
   {
-    name: "K8 Casino",
+    name: "K8casino.io",
     url: "https://k8casino.io",
     status: "online",
     type:'content',
   },
   {
-    name: "K8 Bit",
+    name: "K8bit.io",
     url: "https://k8bit.io",
     status: "online",
     type:'content',
   },
   {
-    name: "K8 Casino1",
+    name: "K8casino1.com",
     url: "https://k8casino1.com",
     status: "online",
     type:'content',
   },
   {
-    name: "K8 Casino2",
+    name: "K8casino2.com",
     url: "https://k8casino2.com",
     status: "online",
     type:'content',
